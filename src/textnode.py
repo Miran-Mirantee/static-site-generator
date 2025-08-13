88d09e535e6aeada9d0a45d1c4d873b4740baab0
Miran-Mirantee <killerm11805@hotmail.com>
2025-08-13 19:55:22 +0700

    implement textNode to leafNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,9 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 class TextType(Enum):
-    PLAIN = "plain"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -9,9 +11,9 @@ class TextType(Enum):
     IMAGE = "image"
     
 class TextNode:
-    def __init__(self, text_type: TextType, text: str, url: str = None):
-        self.text_type = text_type
+    def __init__(self, text: str, text_type: TextType, url: str = None):
         self.text = text
+        self.text_type = text_type
         self.url = url
         
     def __eq__(self, other):
@@ -24,18 +26,20 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
-    def to_html(self) -> str:
-        if self.text_type == TextType.PLAIN:
-            return self.text
-        elif self.text_type == TextType.BOLD:
-            return f"<strong>{self.text}</strong>"
-        elif self.text_type == TextType.ITALIC:
-            return f"<em>{self.text}</em>"
-        elif self.text_type == TextType.CODE:
-            return f"<code>{self.text}</code>"
-        elif self.text_type == TextType.LINK:
-            return f'<a href="{self.text}">{self.text}</a>'
-        elif self.text_type == TextType.IMAGE:
-            return f'<img src="{self.text}" alt="Image">'
-        else:
-            raise ValueError(f"Unknown text type: {self.text_type}")
+def text_node_to_html_node(text_node: TextNode) -> str:
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError(f"Unknown text type: {text_node.text_type}")
+   
