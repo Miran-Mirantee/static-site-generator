b3560a3914bb93f9a350e87d29451c5d253bc721
Miran-Mirantee <killerm11805@hotmail.com>
2025-08-14 15:28:17 +0700

    refactor markdown to blocks

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,2 +1,6 @@
 def markdown_to_blocks(markdown: str) -> list[str]:
-    return list(map(lambda x: "\n".join(list(map(lambda y: y.strip(), x.strip().split("\n")))), markdown.split("\n\n")))
+    return [
+        "\n".join(line.strip() for line in block.strip().split("\n"))
+        for block in markdown.split("\n\n")
+        if block.strip() != ""
+    ]
